ea4eaf4ebeaa8055049a88fe7c4e050d9162fd5d
rahmatj198 <rahmat198@gmail.com>
2023-10-16 07:50:35 +0700

    update apppath for phpgrid
    update apppath for phpgrid

--- app/Http/Controllers/AdmController.php
+++ app/Http/Controllers/AdmController.php
@@ -17,8 +17,10 @@ class AdmController extends Controller
         $mtabid = Menu::find($mid);     //for privileges to grid table 
         $menuid = $mtabid ? $mtabid->id : 0;  
         
-        include_once("phpgrid\config.php");
-        include('phpgrid\jqgrid_dist.php');
+        // include_once("phpgrid\config.php"); 
+        //include('phpgrid\jqgrid_dist.php');
+        include_once(app_path() . "\Http\Controllers\phpgrid\config.php");
+        include(app_path() . "\Http\Controllers\phpgrid\jqgrid_dist.php");
 
         $g = new \jqgrid();
         $g->table = "users";
